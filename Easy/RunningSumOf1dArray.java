0a93b2d1f8c87ea291e1f05a4ed60c189d701b81
Ivan <venssent@mail.ru>
2024-06-26 11:16:33 +0300

    Add 1480. Running Sum of 1d Array

---
+++ Easy/RunningSumOf1dArray.java
@@ -0,0 +1,14 @@
+public class RunningSumOf1dArray {
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i==0){
+                result[i]=nums[i];
+            } else {
+                result[i]=result[i-1]+nums[i];
+            }
+        }
+
+        return result;
+    }
+}
